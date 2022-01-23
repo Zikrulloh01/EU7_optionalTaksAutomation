7b9df424db0ba7e1dfc3cdf04a2a965e1505b629
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-01-23 16:12:13 +0000

    Optional Task Account Summary Tests have been added

--- src/test/java/com/cybertek/tests/tests/ZeroBankTests.java
+++ src/test/java/com/cybertek/tests/tests/ZeroBankTests.java
@@ -6,8 +6,13 @@ import static com.cybertek.utilities.Driver.*;
 
 import com.cybertek.tests.pages.ZeroPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ZeroBankTests extends TestBase {
 
@@ -16,8 +21,6 @@ public class ZeroBankTests extends TestBase {
     @Test(priority = 1)
     public void loginValidDetails(){
         String expectedPage = "Account Summary";
-        getDriver().get(getKey("zeroUrl"));
-        zeroPage.signInButton.click();
         login(getKey("username"), getKey("password"));
         zeroPage.errorbutton.click();
         driver.findElement(By.id("onlineBankingMenu")).click();
@@ -31,8 +34,6 @@ public class ZeroBankTests extends TestBase {
     @Test(priority = 2)
     public void logInInvalidUsername(){
         String expectedTitle = "Zero - Log in";
-        getDriver().get(getKey("zeroUrl"));
-        zeroPage.signInButton.click();
         login(getKey("invalidUsername"), getKey("password"));
         Assert.assertTrue(zeroPage.errorMessage.isDisplayed());
         Assert.assertEquals(driver.getTitle(), expectedTitle, "Title did not match");
@@ -43,8 +44,6 @@ public class ZeroBankTests extends TestBase {
     @Test(priority = 3)
     public void logInInvalidPassword(){
         String expectedTitle = "Zero - Log in";
-        getDriver().get(getKey("zeroUrl"));
-        zeroPage.signInButton.click();
         login(getKey("username"), getKey("invalidPassword"));
         Assert.assertTrue(zeroPage.errorMessage.isDisplayed());
         Assert.assertEquals(driver.getTitle(), expectedTitle, "Title did not match");
@@ -54,8 +53,6 @@ public class ZeroBankTests extends TestBase {
     @Test(priority = 4)
     public void blankUsernameLogin(){
         String expectedTitle = "Zero - Log in";
-        getDriver().get(getKey("zeroUrl"));
-        zeroPage.signInButton.click();
         login(getKey("blankUsername"), getKey("password"));
         Assert.assertTrue(zeroPage.errorMessage.isDisplayed());
         Assert.assertEquals(driver.getTitle(), expectedTitle, "Title did not match");
@@ -65,8 +62,6 @@ public class ZeroBankTests extends TestBase {
     @Test(priority = 5)
     public void blankPasswordLogin(){
         String expectedTitle = "Zero - Log in";
-        getDriver().get(getKey("zeroUrl"));
-        zeroPage.signInButton.click();
         login(getKey("username"), getKey("blankPassword"));
         Assert.assertTrue(zeroPage.errorMessage.isDisplayed());
         Assert.assertEquals(driver.getTitle(), expectedTitle, "Title did not match");
@@ -75,8 +70,6 @@ public class ZeroBankTests extends TestBase {
     @Test(priority = 6)
     public void invalidUsernamePasswordLogin(){
         String expectedTitle = "Zero - Log in";
-        getDriver().get(getKey("zeroUrl"));
-        zeroPage.signInButton.click();
         login(getKey("invalidUsername"), getKey("invalidPassword"));
         Assert.assertTrue(zeroPage.errorMessage.isDisplayed());
         Assert.assertEquals(driver.getTitle(), expectedTitle, "Title did not match");
@@ -86,8 +79,6 @@ public class ZeroBankTests extends TestBase {
     @Test(priority = 7)
     public void blankUsernamePasswordLogin(){
         String expectedTitle = "Zero - Log in";
-        getDriver().get(getKey("zeroUrl"));
-        zeroPage.signInButton.click();
         login(getKey("blankUsername"), getKey("blankPassword"));
         Assert.assertTrue(zeroPage.errorMessage.isDisplayed());
         Assert.assertEquals(driver.getTitle(), expectedTitle, "Title did not match");
@@ -95,7 +86,39 @@ public class ZeroBankTests extends TestBase {
     }
 
 
+
+
+    @Test(priority = 8)
+    public void accountSummaryColumnsTest(){
+        List<String> expectedList = new ArrayList<String>(Arrays.asList("Cash Accounts", "Investment Accounts", "Credit Accounts", "Loan Accounts"));
+        goToAccountSummaryPage();
+        List<WebElement> elements = getDriver().findElements(By.tagName("h2"));
+        for (int i = 0; i < elements.size(); i ++ ) {
+            Assert.assertEquals(elements.get(i).getText(), expectedList.get(i), i + " element did not match");
+        }
+
+    }
+
+
+    @Test
+    public void creditAccountsTest(){
+        List<String> expectedList = new ArrayList<>(Arrays.asList("Account", "Credit Card", "Balance"));
+        goToAccountSummaryPage();
+
+        List<WebElement> elements = driver.findElements(By.xpath("(//*[@class='table'])[3]//th"));
+
+        for (int i = 0; i < elements.size(); i ++ ) {
+            Assert.assertEquals(elements.get(i).getText(), expectedList.get(i), i + " element did not match");
+            System.out.println(elements.get(i).getText());
+        }
+
+    }
+
+
+
     public void login(String username, String password){
+        getDriver().get(getKey("zeroUrl"));
+        zeroPage.signInButton.click();
         zeroPage.usernameField.sendKeys(username);
         zeroPage.passwordField.sendKeys(password);
         zeroPage.loginButton.click();
@@ -104,6 +127,13 @@ public class ZeroBankTests extends TestBase {
     public void logout(){
         zeroPage.usernameIcon.click();
         zeroPage.logoutLink.click();
+    }
+
+    public void goToAccountSummaryPage(){
+        login(getKey("username"), getKey("password"));
+        zeroPage.errorbutton.click();
+        driver.findElement(By.id("onlineBankingMenu")).click();
+        driver.findElement(By.id("account_summary_link")).click();
     }
 
 
